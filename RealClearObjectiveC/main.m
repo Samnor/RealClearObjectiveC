bcc16766ea4dd347df87549f76be72564507e320
Samuel Norling <norling.samuel@gmail.com>
2020-03-09 08:05:22 +0100

    Initial Commit

---
+++ RealClearObjectiveC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RealClearObjectiveC
+//
+//  Created by Samuel Norling on 2020-03-09.
+//  Copyright © 2020 Samuel Norling. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
